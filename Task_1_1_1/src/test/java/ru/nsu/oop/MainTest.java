402732a6a45a325e91bc6eed3393a88261ef0bd6
Art-cher <112766918+Art-cher@users.noreply.github.com>
2026-09-10 12:28:07 +0700

    Task 1 1 1 (#12)
    * final!?
    
    * final!?
    
    ---------
    
    Co-authored-by: Artyom Cherkashin <a.cherkashin2@g.nsu.ru>

--- Task_1_1_1/src/test/java/ru/nsu/oop/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/oop/MainTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.oop;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class mainTest {
+public class MainTest {
     @Test
     void testSortTypical() {
         int[] arr = {5, 4, 3, 2, 1};
